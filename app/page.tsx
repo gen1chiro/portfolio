fade48b16f201cc7bc83a508b5ea26610da93e58
Jul Leo Javellana <juljavellana1218@gmail.com>
2025-11-22 11:43:54 +0800

    chore: setup project fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-full min-h-screen flex justify-center items-center bg-slate-50">
-        This will be my portfolio site.
+    <div className="w-full min-h-screen flex flex-col justify-center items-center bg-slate-50">
+        <h1>This will be my portfolio site.</h1>
+        <p>This site will display all my projects and personal information.</p>
     </div>
   );
 }
