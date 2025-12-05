21f0e574e852cbfde317e0f93aab4718a9bf2eda
Jul Leo Javellana <juljavellana1218@gmail.com>
2025-12-05 09:48:31 +0800

    chore: add contact links

--- components/hero/hero-button.tsx
+++ components/hero/hero-button.tsx
@@ -1,13 +1,17 @@
 import { motion } from 'motion/react';
+import { GoArrowUpRight } from "react-icons/go";
 
 export default function HeroButton() {
     return (
-        <motion.button
+        <motion.a
+            href='/resume.pdf'
+            target='_blank'
             whileHover={{ scale: 1.04 }}
             whileTap={{ scale: 0.98 }}
-            className='bg-glass-dark rounded-full px-6 py-3 text-gray-200 shadow-inner shadow-white'
+            className='flex items-center justify-center bg-glass-dark rounded-full ps-5 pe-4 py-2 text-gray-200 shadow-inner shadow-white'
         >
             Resume
-        </motion.button>
+            <GoArrowUpRight className='mt-1'/>
+        </motion.a>
     )
 }
--- components/shared/nav/nav-button.tsx
+++ components/shared/nav/nav-button.tsx
@@ -3,11 +3,12 @@ import { NavButtonProps } from "@/types";
 
 export default function NavButton({label}: NavButtonProps) {
     return (
-        <motion.button
-            whileHover={{ scale: 1.08 }}
-            whileTap={{ scale: 0.95 }}
+        <motion.a
+            href={'mailto:juljavellana1218@gmail.com?subject=Inquiry%20from%20Portfolio%20Website'}
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.98 }}
             className='bg-glass-white rounded-full px-4 py-1 text-gray-200 shadow-lg shadow-black/30'>
             {label}
-        </motion.button>
+        </motion.a>
     )
 }
