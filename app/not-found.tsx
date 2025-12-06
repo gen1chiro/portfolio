6c6eb586ce32370fc601a3637511c742e875e811
Jul Leo Javellana <juljavellana1218@gmail.com>
2025-12-06 10:39:51 +0800

    feat: 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+
+export default function NotFound() {
+    return (
+        <section className='w-responsive h-full mt-36'>
+            <div>
+                <h1 className='main-text'>Got lost?</h1>
+                <p className='sub-text'>404, Page Not Found.</p>
+            </div>
+        </section>
+    )
+}
