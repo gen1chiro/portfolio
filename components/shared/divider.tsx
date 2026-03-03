dbd7acf6cd2e5793d316eeaa2d7b9c29f4bbb50f
Jul Leo Javellana <juljavellana1218@gmail.com>
2026-03-03 12:31:34 +0800

    chore: update divider

--- components/shared/divider.tsx
+++ components/shared/divider.tsx
@@ -2,7 +2,7 @@
 export default function Divider() {
     return (
         <div className='w-full font-semibold text-sm text-gray-400'>
-            . . .
+            ` ` `
         </div>
     )
 }
