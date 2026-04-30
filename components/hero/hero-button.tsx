5b259f9e9ed77875cee8731a5fa186eba3b7328b
Jul Leo Javellana <juljavellana1218@gmail.com>
2026-04-30 14:14:16 +0800

    chore: temporarily disable resume button

--- components/hero/hero-button.tsx
+++ components/hero/hero-button.tsx
@@ -4,8 +4,8 @@ import { GoArrowUpRight } from "react-icons/go";
 export default function HeroButton() {
     return (
         <motion.a
-            href='/resume.pdf'
-            target='_blank'
+            // href='/resume.pdf'
+            // target='_blank'
             whileHover={{ scale: 1.04 }}
             whileTap={{ scale: 0.98 }}
             className='flex items-center justify-center bg-glass-dark rounded-full ps-5 pe-4 py-2 text-gray-200 shadow-inner shadow-white'
